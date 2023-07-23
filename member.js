4ab20a84a901cf7dd6b660429354c4b45a386883
Andreas Noll <49374982+ajrnoll@users.noreply.github.com>
2023-07-23 10:22:05 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,19 @@
+function skillsMember() {
+    var member = {
+        name: 'John Doe',
+        age: 30,
+        address: '123 Main St'
+    };
+
+    var skills = {
+        languages: ['JavaScript', 'Ruby', 'Python'],
+        isDesigner: true,
+        isDeveloper: true,
+        isArtist: false
+    };
+
+    // Add your code below this line
+    member.skills = skills;
+    return member;
+    // Add your code above this line
+}
